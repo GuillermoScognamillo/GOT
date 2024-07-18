a161b2dc72a5a992cec4d54a62d64a4120fa3d22
Guillermo <guillermoscognamillo86@gmail.com>
2024-07-17 21:18:56 -0300

    cursor molestar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const $body = document.querySelector("body");
 const $boton = document.querySelector("button");
 
+
 const removerBoton = () => {
     $boton.classList.remove('btn-molestar');
     $boton.classList.add('oculto');
@@ -14,34 +15,40 @@ const mostrarBoton = () => {
 const molestarDrogon = () => {
     if ($body.classList.contains('verde')){
         removerBoton();
+        $body.classList.add("tocar");
         $body.classList.remove('verde');
         $body.classList.add('tAmarillo');
         setTimeout(()=>{
             mostrarBoton();
             $body.classList.remove('tAmarillo');
             $body.classList.add('Amarillo');
+            $body.classList.remove("tocar");
         } ,3000);
         $boton.innerText='Seguir molestando'
         }
     else if ($body.classList.contains('Amarillo')){
         removerBoton();
+        $body.classList.add("tocar");
         $body.classList.remove('Amarillo');
         $body.classList.add('tNaranja');
         setTimeout(()=>{
             mostrarBoton();
             $body.classList.remove('tNaranja');
             $body.classList.add('Naranja');
+            $body.classList.remove("tocar");
         } ,2000);
         $boton.innerText='BOOP BOOP'
     }
     else if ($body.classList.contains('Naranja')){
         removerBoton();
+        $body.classList.add("tocar");
         $body.classList.remove('Naranja');
         $body.classList.add('tRojo');
         setTimeout(()=>{
             mostrarBoton();
             $body.classList.remove('tRojo');
             $body.classList.add('Rojo');
+            $body.classList.remove("tocar");
         } ,3000);
         $boton.innerText='Estas muerto, revivir'
     }
